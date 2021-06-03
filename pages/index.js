40dc3af4179313ac20757dccbde6f4f048d821c3
Hedakb <85231241+Hedakb@users.noreply.github.com>
2021-06-03 22:10:53 +0200

    Test

--- pages/index.js
+++ pages/index.js
@@ -4,3 +4,8 @@ import { useAuth } from "../context/auth.tsx";
 export default function Home() {
   return <div>"Hei hei, dette blir nettsiden"</div>;
 }
+
+
+# Git pull 
+# git commit -m "la til kode som ikke virker"
+# git push
